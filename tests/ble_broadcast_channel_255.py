6cb2c01099ac5279990cb718806dd8df6cc696cb
Bert <BertLindeman@users.noreply.github.com>
2026-02-21 23:19:40 +0100

    show hub is still connected
    Added color indication for Bluetooth connection status.

--- tests/ble_broadcast_channel_255.py
+++ tests/ble_broadcast_channel_255.py
@@ -3,7 +3,9 @@
 broadcast_interval = 250
 
 from pybricks.hubs import ThisHub
+from pybricks.parameters import Color 
 from pybricks.tools import wait, StopWatch
+
 hub = ThisHub(broadcast_channel=255)
 watch = StopWatch()
 XMIT_DATA_LIST = [1, 2028, "String3", [4,8,12,16], ('5 tupleA', '5 tupleB')]
@@ -13,4 +15,12 @@ while True:
     for xmit_data in XMIT_DATA_LIST:
         watch.reset()
         hub.ble.broadcast(xmit_data)
+
+        # Hub light will be RED if connected to a PC via bluetooth.
+        # After disconnect (it takes a few seconds) the light goes to GREEN.
+        if hub.system.info()['host_connected_ble']:
+            hub.light.on(Color.RED)  # show RED if connected
+        else:
+            hub.light.on(Color.GREEN)
+
         wait(broadcast_interval - watch.time())
